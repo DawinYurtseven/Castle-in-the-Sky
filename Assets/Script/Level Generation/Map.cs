3b5e8347cc01fe6ba2a31c314e6495b457c6ca96
Dawin Yurtseven <dawinyurtseven@gmail.com>
2026-05-25 14:55:56 +0200

    starting proper implementation
    I will overlook all visuals as I want it to work functionally

--- Assets/Script/Level Generation/Map.cs
+++ Assets/Script/Level Generation/Map.cs
@@ -9,21 +9,43 @@ public class Map : MonoBehaviour
     
     public List<PlayerUnit> currentPlayerUnits;
     
+    public List<Node> nodes = new List<Node>();
+    public Node currentNode;
+
+    [SerializeField] private List<GameObject> systems = new ();
+    [SerializeField] private GameObject mapImage, nodePrefab;
+    [SerializeField] private Vector3 startPos;
+    [SerializeField] private float width;
+    [SerializeField] private float depth;
+    [SerializeField] private int levelDepth;
+    
+    
     
 
     private void Awake()
     {
         if(!system) system = this;
         else Destroy(this);
+
+        ReturnToMap();
     }
 
     public void GameStart(List<PlayerUnit> playerUnits)
     {
         currentPlayerUnits = playerUnits;
+        
+        currentNode = Instantiate(nodePrefab, startPos, Quaternion.identity, transform).GetComponent<Node>();
+        nodes.Add(currentNode);
+        
+        //Spawn new nodes or do it after completion
     }
 
     public void ReturnToMap()
     {
-        
+        foreach (GameObject go in systems)
+        {
+            go.SetActive(false);
+        }
+        gameObject.SetActive(true);
     }
 }
